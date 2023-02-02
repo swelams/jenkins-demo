7b0c5a84c5a0434d1dc7676e01952c97a420d39c
mohamed-swelam <mohamed.swelam@vodafone.com>
2023-02-02 17:04:40 +0200

    last lec

---
+++ agent.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+apt update
+apt install default-jdk -y
+apt install apt-transport-https -y && apt install ca-certificates -y &&  apt install curl -y && apt install software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt install docker-ce -y
+systemctl start docker
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+usermod -aG docker ubuntu
